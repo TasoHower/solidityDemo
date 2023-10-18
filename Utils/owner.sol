5f2c2cf6b47f72905a83de36017abc972b3fce98
TasoHower <tasohower@gmail.com>
2023-10-19 00:08:29 +0800

    feat: Owner&pause&safeMath

---
+++ Utils/owner.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.0;
+
+contract ownerable {
+    address _owner;
+
+    constructor() {
+        _owner = msg.sender;
+    }
+
+    modifier onlyOwner {
+        require(msg.sender == _owner);
+        _;
+    }
+
+    event ownerTransfed(address older,address newOwner);
+
+    function transferOwner(address newOwner) public onlyOwner {
+        _owner = newOwner;
+        emit ownerTransfed(msg.sender,newOwner);
+    }
+}
---
+++ Utils/pausable.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.0;
+
+import "./owner.sol";
+
+contract Pausable is ownerable{
+    bool _pause;
+    constructor() {
+        _pause = false;
+    }
+
+    modifier onlyPaused() {
+        require(_pause,"only paused");
+        _;
+    }
+
+    modifier onlyNotpaused(){
+        require(!_pause,"only not pause");
+        _;
+    }
+
+    event Pause();
+    event Unpause();
+
+    function pause() public onlyNotpaused onlyOwner{
+        _pause = true;
+    }
+
+    function unpause() public onlyPaused onlyOwner{
+        _pause = false;
+    }
+}
