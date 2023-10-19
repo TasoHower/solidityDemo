99f2f2f3266a990cc78c8ff434f314dfb885853f
TasoHower <tasohower@gmail.com>
2023-10-19 14:25:43 +0800

    feat: ERC-721

--- Utils/owner.sol
+++ Utils/owner.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.0;
+pragma solidity 0.8.19;
 
 contract ownerable {
     address _owner;
@@ -8,15 +8,15 @@ contract ownerable {
         _owner = msg.sender;
     }
 
-    modifier onlyOwner {
+    modifier onlyOwner() {
         require(msg.sender == _owner);
         _;
     }
 
-    event ownerTransfed(address older,address newOwner);
+    event ownerTransfed(address older, address newOwner);
 
     function transferOwner(address newOwner) public onlyOwner {
         _owner = newOwner;
-        emit ownerTransfed(msg.sender,newOwner);
+        emit ownerTransfed(msg.sender, newOwner);
     }
 }
