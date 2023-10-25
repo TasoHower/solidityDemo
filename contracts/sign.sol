3d1c928f4bce0605a2b0df595f8d635babbc8c6a
TasoHower <tasohower@gmail.com>
2023-10-25 17:01:17 +0800

    style: format code

--- contracts/sign.sol
+++ contracts/sign.sol
@@ -5,7 +5,7 @@ import "../Utils/owner.sol";
 
 /**
  * 注意：代码未经测试，请勿直接商用
- * 
+ *
  * Code by: Tasohower(https://github.com/TasoHower)
  * 部署地址： https://goerli.etherscan.io/address/0x2e44930fa9d407e814dc774f5714c0db9097b26b#code（已开源）
  */
@@ -88,7 +88,7 @@ contract Sign is ownerable {
         uint256 lastTime,
         uint256 _requireVotes,
         address to
-    )  public payable onlyMember {
+    ) public payable onlyMember {
         require(_requireVotes > 0);
         bytes32 random = keccak256(
             abi.encodePacked(msg.sender, _callData, block.timestamp)
@@ -108,16 +108,28 @@ contract Sign is ownerable {
 
         transferPool[random] = newTP;
 
-        emit TransferAdded(random,msg.sender, lastTime, _callData, _requireVotes);
+        emit TransferAdded(
+            random,
+            msg.sender,
+            lastTime,
+            _callData,
+            _requireVotes
+        );
     }
 
     function vote(bytes32 proposal) public onlyMember {
         // 必须是创建之前存在的 member
-        require(memberJoinTime[msg.sender] < transferPool[proposal].created_at,"must useful member");
+        require(
+            memberJoinTime[msg.sender] < transferPool[proposal].created_at,
+            "must useful member"
+        );
         // 必须没有投过票
-        require(!transferVoted[proposal][msg.sender],"must not voted");
+        require(!transferVoted[proposal][msg.sender], "must not voted");
         // 交易必须有效
-        require(transferPool[proposal].expire_at > block.timestamp,"transfer must useful");
+        require(
+            transferPool[proposal].expire_at > block.timestamp,
+            "transfer must useful"
+        );
 
         TransactionProposal memory tp = transferPool[proposal];
         tp.agreed += 1;
